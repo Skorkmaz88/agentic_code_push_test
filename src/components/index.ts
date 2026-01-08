a083e83fe4bee9fc6174e8c2eb31373ae2f828ec
Semih Korkmaz <contactsemih@gmail.com>
2026-01-08 15:55:02 +0200

    Add main components index file for easy imports

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+// Export all UI components
+export * from './Button';
+export * from './Input';
+export * from './Card';
+export * from './Modal';
+export * from './Navbar';
+
+// Re-export types for convenience
+export type { ButtonProps } from './Button';
+export type { InputProps } from './Input';
+export type { CardProps } from './Card';
+export type { ModalProps } from './Modal';
+export type { NavbarProps, MenuItem, User } from './Navbar';
